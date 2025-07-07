43d38b5203461c8f6f1d155e797bb76a43b0d0dc
Sachin Vaghela <sachinvaghela27@gmail.com>
2025-07-07 23:55:27 +0530

    Update hard-49-credit-card-transactions(Part-1).sql

--- hard-49-credit-card-transactions(Part-1).sql
+++ hard-49-credit-card-transactions(Part-1).sql
@@ -28,3 +28,23 @@ group by city)
 select city, first_transaction_date, tran_date_1500, datediff(tran_date_1500,first_transaction_date) as no_of_days
 from cte2
 order by city
+
+-- Alternate Solution
+
+with cte as 
+(select city, transaction_date, 
+sum(amount) as total_amount, min(transaction_date)over(partition by city) as first_transaction_date
+from credit_card_transactions
+group by city, transaction_date)
+, cte2 as
+(select city, transaction_date, first_transaction_date,
+sum(total_amount)over(partition by city order by transaction_date rows between unbounded preceding and current row) as cum_sum
+from cte)
+
+select city, first_transaction_date, 
+min(transaction_date) as tran_date_1500, 
+min(transaction_date)-first_transaction_date as no_of_day
+from cte2
+where cum_sum >= 1500
+group by city,first_transaction_date
+order by city
