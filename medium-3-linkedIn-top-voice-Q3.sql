9bec7c1a540bfb9aefd243996949718cf4fe959d
Sachin Vaghela <sachinvaghela27@gmail.com>
2025-08-10 15:54:23 +0530

    Update medium-3-linkedIn-top-voice-Q3.sql

--- medium-3-linkedIn-top-voice-Q3.sql
+++ medium-3-linkedIn-top-voice-Q3.sql
@@ -1,5 +1,6 @@
 -- LinkedIn is a professional social networking app. 
--- They want to give top voice badge to their best creators to encourage them to create more quality content. A creator qualifies for the badge if he/she satisfies following criteria.
+-- They want to give top voice badge to their best creators to encourage them to create more quality content. 
+-- A creator qualifies for the badge if he/she satisfies following criteria.
 -- 1- Creator should have more than 50k followers.
 -- 2- Creator should have more than 100k impressions on the posts that they published in the month of Dec-2023.
 -- 3- Creator should have published atleast 3 posts in Dec-2023.
